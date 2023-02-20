dbfcc8a29b4aaef09e7fed9638ff59d0dd71016e
ethzhang <ethanzhang.cn@gmail.com>
2023-02-20 22:42:07 +0800

    add google tag manager script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+            `}
+      </Script>
+
       <Component {...pageProps} />
       <Analytics />
     </>
